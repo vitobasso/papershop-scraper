d5e6f616b91c76445160e6dbe2851058d1c49234
Victor Basso <vitobasso@gmail.com>
2017-03-22 22:20:14 +0100

    reorganized files

--- src/mapping-loader.js
+++ src/mapping-loader.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 exports.load = (site) => {
     var fileName = site + '.json'
-    var jsonPath = path.join(__dirname, 'mappings', fileName);
+    var jsonPath = path.join(__dirname, '..', 'mappings', fileName);
     var content = fs.readFileSync(jsonPath, 'utf8');
     return JSON.parse(content)
 }
