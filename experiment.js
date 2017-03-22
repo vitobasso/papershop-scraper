d66ebce0ab5cc165128161da4c930f221e18ac1d
Victor Basso <vitobasso@gmail.com>
2017-03-22 20:33:30 +0100

    extracted mapping. refactored.

--- experiment.js
+++ experiment.js
@@ -3,53 +3,76 @@ var webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-var driver = new webdriver.Builder().forBrowser('chrome').build();
+var capabilities = {
+    prefs: {
+       profile: {
+           managed_default_content_settings: {
+               images: 2
+           }
+       }
+    }
+}
 
+var driver = new webdriver.Builder()
+    .withCapabilities(capabilities)
+    .forBrowser('chrome')
+    .build()
 
-//driver.get('http://www.ebay.com');
-//driver.findElement(By.xpath("//*[@id='gh-ac']")).sendKeys('Android');
-//driver.findElement(By.xpath("//*[@id='gh-btn']")).click();
-//driver.wait(until.elementLocated(By.xpath(container)), 5000);
+var mapping = {
+    actions: [
+        {type: "url", value: "http://www.ebay.com/"},
+        {type: "click", target: "//*[@id='gh-ac']"},
+        {type: "text", target: "//*[@id='gh-ac']", value: "Android"},
+        {type: "click", target: "//*[@id='gh-btn']"}
+    ],
+    structure: {
+        container: "/html/body/div[5]/div[2]/div[1]/div[1]/div/div[1]/div/div[3]/div/div[1]/div/w-root/div/div/ul",
+        itemPattern: "/li[*4]", // expandable node
+        fields: {
+            title: "/h3/a",
+            price: "/ul/li/span",
+            from: "/ul[2]/li",
+            type: "/ul/li[2]/span",
+            photo: "/div/div/a/img"
+        }
+    }
+}
 
-var actions = [
-    {type: "url", value: "http://www.ebay.com/"},
-    {type: "click", target: "//*[@id='gh-ac']"},
-    {type: "text", target: "//*[@id='gh-ac']", value: "Android"},
-    {type: "click", target: "//*[@id='gh-btn']"}
-]
+var itemPathPattern = mapping.structure.container + mapping.structure.itemPattern
+var itemPaths = expandPath(itemPathPattern)
 
-var container = "/html/body/div[5]/div[2]/div[1]/div[1]/div/div[1]/div/div[3]/div/div[1]/div/w-root/div/div/ul"
-var itemPattern = "/li[*4]" // expandable node
-var title = "/h3/a"
-var price = "/ul/li/span"
-var from = "/ul[2]/li"
-var type = "/ul/li[2]/span"
-var photo = "/div/div/a/img"
+var actionPromises = mapping.actions.map(scheduleAction)
+Promise.all(actionPromises).then(() => {
+    console.log('after action promises')
+    itemPaths.forEach(showFields)
+})
 
+//driver.quit();
 
-var itemXPaths = expandPath(container+itemPattern)
-itemXPaths.forEach(showFields)
-
-function fireAction(action){
+function scheduleAction(action){
+    console.log('scheduleAction', action)
     if(action.type == 'url') return driver.get(action.value)
     if(action.type == 'click') return driver.findElement(By.xpath(action.target)).click();
     if(action.type == 'text') return driver.findElement(By.xpath(action.target)).sendKeys(action.value);
     console.err('unknown action', action)
 }
 
-function showFields(itemXPath) {
-//    var itemXPath = container + item(i)
-    var textVal = driver.findElement(By.xpath(itemXPath + title)).getText()
-    var priceVal = driver.findElement(By.xpath(itemXPath + price)).getText()
-    var fromVal = driver.findElement(By.xpath(itemXPath + from)).getText()
-    var typeVal = driver.findElement(By.xpath(itemXPath + type)).getText()
-    var photoVal = driver.findElement(By.xpath(itemXPath + photo)).getAttribute("src")
+function showFields(itemPath) {
+    console.log('showFields', itemPath)
+    var textVal = find('title').getText()
+    var priceVal = find('price').getText()
+    var fromVal = find('from').getText()
+    var typeVal = find('type').getText()
+    var photoVal = find('photo').getAttribute("src")
     Promise.all([textVal, priceVal, fromVal, typeVal, photoVal]).then(
         (values) => console.log(values)
     )
-}
 
-//driver.quit();
+    function find(field){
+        var fieldPath = itemPath + mapping.structure.fields[field]
+        return driver.findElement(By.xpath(fieldPath))
+    }
+}
 
 // ------------------------------- expand xpath -----------------------------------------
 
@@ -94,81 +117,3 @@ function prependToAll(prefix, suffixes){
 function flatten(xss) {
     return Array.prototype.concat.apply([], xss);
 };
-
-// ------------------------------- expand xpath (previous impl) -----------------------------------------
-
-// generates xpaths via index combinations for given original xpath
-// e.g., given:     /div/div[]/ul/li[] (can have any indexes filled in)
-//       returns:   /div/div[1]/ul/li[1]
-//                  /div/div[1]/ul/li[2]
-//                  /div/div[2]/ul/li[1]
-//                  /div/div[2]/ul/li[2]
-//                  ...
-// "originalParts" is the given xpath split by "/"
-//function expandXPath(prefix, suffixPattern){
-//    var parts = suffixPattern.split('/')
-//        .slice(1) //remove empty node before trailing "/"
-//    return expandXPathSuffixes(prefix, parts).then(suffixesParts =>
-//        suffixesParts
-//            .map((parts) => parts.join('/'))
-//            .map((s) => prefix + '/' + s)
-//    )
-//
-//    function expandXPathSuffixes(prefix, originalParts, depth=0){
-//        if(originalParts.length <= 0) return [] //TODO promise
-//        if(prefix && !evaluateXPath(prefix)) return [] //TODO promise
-//
-//        var originalHead = originalParts[0]
-//        var originalTail = originalParts.slice(1)
-//
-//        if(!originalHead.match(indexRegex)) {
-//            return recursiveCall(prefix, originalHead, originalTail, depth) // recursion
-//        }
-//
-//        var callForHead = (ithHead) =>
-//            recursiveCall(prefix, ithHead, originalTail, depth) // recursion
-//        return iterateXPathIndex(prefix, originalHead, callForHead)
-//    }
-//
-//    function recursiveCall(prefix, head, tail, depth) {
-//        if(tail.length <= 0) return [[head]] //TODO promise
-//
-//        var tailPrefix = prefix + '/' + head
-//        expandXPathSuffixes(tailPrefix, tail, depth+1).then(newTails => { // recursion
-//            var newParts = prependToAll(head, newTails)
-//            return newParts
-//        })
-//    }
-//}
-//
-//
-//// generates indexes to "current"
-//// e.g.: given "div[]", generates "div[1]", "div[2]", etc
-//// counts prefix's children to tell when to stop
-//function iterateXPathIndex(prefix, current, callback){
-//    var tag = current.replace(indexRegex, '')
-//    return countChildTags(prefix, tag).then(total => {
-//        var results = []
-//        for(var i=1; i<=total; i++){
-//            var indexed = replaceFirstXPathIndex(current, i)
-//            var ithResult = callback(indexed) //TODO callback returns promise
-//            results = results.concat(ithResult)
-//        }
-//        return results;
-//    })
-//}
-//
-////replaces only the first occurrence
-//function replaceFirstXPathIndex(xpath, i){
-//    var replacement = '[' + i + ']'
-//    return xpath.replace(indexRegex, replacement)
-//}
-//
-//function countChildTags(xpath, tagName){
-//    driver.findElement(By.xpath(xpath)).then(node => {
-//        if(!node) return 0
-//        var children = [...node.children]
-//        var hasSameTag = (n) => n.tagName == tagName.toUpperCase()
-//        return children.filter(hasSameTag).length
-//    })
-//}
