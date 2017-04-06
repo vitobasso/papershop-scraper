f08667427ecdcbf7beadb743d73ce1449b71a1d1
Victor Basso <vitobasso@gmail.com>
2017-04-06 07:40:33 +0200

    ebay fix

--- src/scraper.js
+++ src/scraper.js
@@ -7,7 +7,7 @@ var webdriver = require('selenium-webdriver'),
     promise = webdriver.promise
 var driver = require('./driver/firefox').driver
 
-var site = loader.load('marktplaats')
+var site = loader.load('ebay')
 var log = s => () => console.log(s)
 
 prepare().then(() => console.log('page ready'))
